9c18f1ce9d563db27351525b947b89f028a7cf84
lucasyarza <ldeyarza@mercadona.es>
2022-12-08 09:24:49 +0100

    Highlighting

--- after/plugin/highlight.lua
+++ after/plugin/highlight.lua
@@ -3,6 +3,7 @@ local util = require('user.colors.util')
 local highlights = {
   ['@function.call'] = {italic = true},
   ['@keyword'] = {italic = true},
+  ['@keyword.function'] = {italic = true},
 }
 
 for hlgroup, hlcolors in pairs(highlights) do
